0812a0f2fb187d55ccf988c81c7a5e7caaeaac9e
Prachi mehra <prachimehra259@gmail.com>
2026-07-28 18:42:14 +0530

    initial commit

---
+++ homework.md
@@ -0,0 +1,11 @@
+create a repository 
+Initialize the repository 
+node_modules , package.json , package-lock.json 
+install express
+create a server 
+listen to port 7777
+write a request handlers for /test , /hello
+install nodemon and update scripts inside package.json
+what are dependencies 
+what is the use of "-g" while npm install
+diff between caret and tilde
---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+
+app.use("/hello",(req, res) => {
+    res.send("namaste from the dashboards !");
+}
+)
+app.use("/",(req, res) => {
+    res.send("namaste from the dashboards !");
+}
+)
+app.use("/test",(req, res) => {
+    res.send("Hello from the server!");
+}
+)
+
+app.listen(3000, () =>  {
+    console.log("Server is successfully listening on port 3000");
+});

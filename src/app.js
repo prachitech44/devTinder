250fedfca9e3c04641c789ad5409c29426b44d6a
prachitech44 <prachimehra259@gmsil.com>
2026-08-05 18:24:37 +0530

    multiple route handler

--- src/app.js
+++ src/app.js
@@ -2,11 +2,36 @@ const express = require('express');
 
 const app = express();
 
-app.get("/user/:userID/:name?passwprd", (req, res) =>{
-    console.log(req.params);
-    res.send({firstName: "Prachi", lastname: "Mehra"});
+//app.use("/route" , rH, [rH2 ,rH3] , rh4, rH5);
 
-});
+app.use(
+    "/user",
+    (re3q , res , next) =>{
+        console.log("Handling the route user!!");
+        //res.send("Response!!");
+        next();
+    },
+    (req , res , next) => {
+        console.log("Handling the route user 2 !!");
+        //res.send("2nd response");
+        next();
+    },
+     (req , res , next) => {
+        console.log("Handling the route user 3 !!");
+        //res.send("3rd response");
+        next();
+    },
+     (req , res, next) => {
+        console.log("Handling the route user 4 !!");
+        //res.send("4th response");
+        next();
+    },
+    (req , res , next) =>{
+        console.log("handling the route user");
+        res.send("5th response");
+    }
+     
+);
 
 app.listen(7777, () =>  {
     console.log("Server is successfully listening on port 7777");
